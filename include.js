399303ed77080be41208a4c28afc49dc47c309bd
DESKTOP-GTKSC19\gme <youngho7482@naver.com>
2023-05-02 18:52:23 +0900

    20230428_develop

--- include.js
+++ include.js
@@ -73,3 +73,44 @@ tabItem.forEach((tab, idx) => {
     tabInner[idx].classList.add("active");
   });
 });
+
+// variables
+var accordionBtn = document.querySelectorAll(".headTitle");
+var allTexts = document.querySelectorAll(".text");
+var accIcon = document.querySelectorAll(".accIcon");
+
+// event listener
+accordionBtn.forEach(function (el) {
+  el.addEventListener("click", toggleAccordion);
+});
+
+// function
+function toggleAccordion(el) {
+  var targetText = el.currentTarget.nextElementSibling.classList;
+  var targetAccIcon = el.currentTarget.children[0];
+  var target = el.currentTarget;
+
+  if (targetText.contains("show")) {
+    targetText.remove("show");
+    targetAccIcon.classList.remove("anime");
+    target.classList.remove("accordionTitleActive");
+    target.classList.remove("headTitleActive");
+  } else {
+    accordionBtn.forEach(function (el) {
+      el.classList.remove("accordionTitleActive");
+      el.classList.remove("headTitleActive");
+      allTexts.forEach(function (el) {
+        el.classList.remove("show");
+      });
+
+      accIcon.forEach(function (el) {
+        el.classList.remove("anime");
+      });
+    });
+
+    targetText.add("show");
+    target.classList.add("accordionTitleActive");
+    target.classList.add("headTitleActive");
+    targetAccIcon.classList.add("anime");
+  }
+}
